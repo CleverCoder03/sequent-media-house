f54bebe435ac53ef634672a0c7bd7257325d1b34
Senpai <clevercoder0307@gmail.com>
2026-02-07 08:50:56 +0530

    fix need proof section -v3

--- src/components/NeedProof.jsx
+++ src/components/NeedProof.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import gsap from "gsap";
-// import ScrollTrigger from "gsap/ScrollTrigger";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
+// import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
